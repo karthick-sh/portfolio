8cd773a8d12f3a00cc16822c03dac8ccf4f74b67
Karthick Shankar <k.shankar@salesforce.com>
2025-06-06 12:05:20 -0700

    feat: add netlify plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import netlify from "@netlify/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlify()],
   server: {
     port: 3000,
     open: true
   },
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   }
 }) 
